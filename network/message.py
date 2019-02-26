68bf8879854dfcbc4a339cf78fc29d851516014d
lihaibo3653 <lihaibo3653@dingtalk.com>
2019-02-26 19:48:48 +0800

    modify message

--- network/message.py
+++ network/message.py
@@ -1,9 +1,26 @@
-import socket,json
+import socket,json,struct
+
+
+#发送聊天信息
+def send_heartbeat(s:socket.socket,time):
+    send_object = dict()
+    send_object['a'] = 'heartbeat'
+    send_object['time'] = time
+    send_data = json.dumps(send_object)
+    print('发送的消息:',send_object)
+    send_data = send_data.encode('utf-8')
+    send_pack(s,send_data)
+
+
+def send_pack(s:socket.socket,send_data):
+    l = len(send_data)
+    send_bytes = struct.pack('H',l)
+    send_bytes = send_bytes + send_data
+    s.send(send_bytes)
+
 
 def send_register(s,username,password,email):
     pass
-
-
 
 #发送验证信息
 def send_auth(s,username,password):
@@ -13,7 +30,7 @@ def send_auth(s,username,password):
     send_object['password'] = password
     send_data  = json.dumps(send_object)
     send_data = send_data.encode('utf-8')
-    s.send(send_data)
+    send_pack(s,send_data)
 
 #发送聊天信息
 def send_message(s:socket.socket,message,to_uid):
@@ -24,7 +41,7 @@ def send_message(s:socket.socket,message,to_uid):
     send_data = json.dumps(send_object)
     print('发送的消息:',send_object)
     send_data = send_data.encode('utf-8')
-    s.send(send_data)
+    send_pack(s,send_data)
 
 
 def send_client_message(s:socket.socket,message,from_uid):
@@ -35,4 +52,4 @@ def send_client_message(s:socket.socket,message,from_uid):
     send_data = json.dumps(send_object)
     print('[send_client_message]发送的消息:',send_object)
     send_data = send_data.encode('utf-8')
-    s.send(send_data)
+    send_pack(s,send_data)
